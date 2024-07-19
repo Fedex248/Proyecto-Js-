d7f1ebc67a787c0eee47aea96a87a3699bf9889b
Federico Santamaria <fedesanta2020@gmail.com>
2024-07-18 23:46:16 -0300

    proyecto final

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ function mostrarDatosCuenta() {
 // Evento de registro
 const formularioRegistro = document.getElementById('formularioRegistro');
 if (formularioRegistro) {
-    formularioRegistro.addEventListener('submit', function(event) {
+    formularioRegistro.addEventListener('submit', function (event) {
         event.preventDefault();
 
         const nombre = document.getElementById('nombre').value;
@@ -67,7 +67,7 @@ if (formularioRegistro) {
 // Evento de inicio de sesión
 const formularioInicioSesion = document.getElementById('formularioInicioSesion');
 if (formularioInicioSesion) {
-    formularioInicioSesion.addEventListener('submit', function(event) {
+    formularioInicioSesion.addEventListener('submit', function (event) {
         event.preventDefault();
 
         const nombreUsuario = document.getElementById('nombreUsuario').value;
@@ -113,7 +113,7 @@ function buscarVideojuego(nombre) {
     return null;
 }
 
-//let nombreBusqueda = prompt("Por favor, ingresa el nombre del videojuego que deseas buscar:");
+//let nombreBusqueda = prompt("Por favor, ingresa el nombre del videojuego que deseas buscar:");`
 //let videojuegoBuscado = buscarVideojuego(nombreBusqueda);
 
 if (videojuegoBuscado) {
@@ -121,3 +121,104 @@ if (videojuegoBuscado) {
 } else {
     console.log(`Lo siento, "${nombreBusqueda}" no está disponible en nuestra tienda.`);
 }
+
+// carrito
+
+const baseDeDatos = [
+
+    {
+        id: 1,
+        nombre: 'memoria ram',
+        precio: 1000,
+        imagen: 'imagen.jpg'
+    },
+    {
+        id: 2,
+        nombre: 'mother',
+        precio: 2000,
+        imagen: 'imagen.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'teclado',
+        precio: 3000,
+        imagen: 'imagen.jpg'
+    }
+
+]
+
+let carrito = [];
+
+const DOMitem = document.getElementById('items');
+const DOMcarrito = document.getElementById('carrito');
+const DOMtotal = document.getElementById('total');
+const DOMbotonvaciar = document.getElementById('botonvaciar')
+
+//funciones
+
+function renderizarProductos() {
+    baseDeDatos.forEach((info) => {
+        const html = `
+        <div class="card" style="width: 18rem;">
+        <img src="${info.imagen}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${info.nombre}</h5>
+        <p class="card-text">$: ${info.precio}</p>
+        <button class="btn btn-primary" marcador="${info.id}">comprar</button>
+</div>
+</div>  
+        `;
+        DOMitem.innerHTML += html;
+    })
+    document.querySelectorAll('.btn .btn-primary').forEach((boton) => {
+        boton.addEventListener('click', agregarProductoCarrito)
+    })
+
+}
+
+//agregar producto al carrito
+
+function agregarProductoCarrito(evento) {
+    carrito.push(evento.target.getAttribute('marcador'))
+
+    console.log(carrito)
+    //
+    renderizarCarrito();
+    //local storage
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    //trabajr con sweetalert
+    Swal.fire({
+        title: "agregado a carrito",
+        text: "el producto fue sumando a su pedido",
+        icon: "success",
+        confirmButtonText: "aceptar"
+    })
+
+}
+
+function renderizarCarrito() {
+    DOMcarrito.innerHTML = '';
+    carrito.forEach((id) => {
+        const producto = baseDeDatos.find((info) => info.id === parseInt(id))
+        const html = `
+                    <li>
+                        ${producto.nombre} - ${producto.precio}
+                    </li>
+
+        `
+        DOMcarrito.innerHTML += html;
+    })
+    const total = carrito.reduce((total, id) => {
+        const producto = baseDeDatos.find((info) => info === parseInt(id))
+        return total + producto.precio;
+    }, 0)
+    DOMtotal.textContent = total.toFixed(2)
+}
+
+renderizarProductos()
+
+if (localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito') || [])
+    renderizarCarrito()
+}
+
